00b525373c4ba9042f98c2a1f64bee2ae84f74c5
zhi.zhou <zhi.zhou@chaitin.com>
2016-04-30 16:04:25 +0800

    fix npm error and clean up callbacks

--- install.js
+++ install.js
@@ -1,42 +1,76 @@
+// For the max compatibility, avoid using ES6 features like Promise, let or const
+
 var http = require('http')
 var fs = require('fs')
 var spawn = require('child_process').spawn
+var npm = require('npm')
 
-var tmp = '17monipdb.zip',
-  db = '17monipdb.dat'
+var TMP = '17monipdb.zip',
+  DB = '17monipdb.dat',
+  URL = 'http://s.qdcdn.com/17mon/17monipdb.zip'
 
-function success() {
-  console.info('Successfully download and extract ip database (free version)')
-  fs.unlink(tmp, process.exit)
+
+function download(callback) {
+  http.get(URL, function(response) {
+    response.pipe(fs.createWriteStream(TMP)).on('finish', function() {
+        console.info('Database Successfully downloaded.')
+        callback()
+      })
+      .on('error', callback)
+  })
 }
 
-fs.unlink(db, function() {
-  http.get('http://s.qdcdn.com/17mon/17monipdb.zip', function(response) {
-    response.pipe(fs.createWriteStream(tmp)).on('finish', function() {
-      console.info('Database download finished.')
-        // unzip to current directory
-      spawn('unzip', ['-j', tmp, db, '-d', __dirname])
-        .on('error', function() {
-          console.warn('zip command not found, fallback to node-unzip')
-            // for those system who doesn't have unzip util
-          spawn('npm', ['install', 'unzip'])
-            .on('exit', function(code, signal) {
-              if (code) {
-                console.error('Unabled to install unzip util, ' +
-                  'you have to manually download database to make the module work')
-              } else {
-                fs.createReadStream(tmp)
-                  .pipe(require('unzip').Parse())
-                  .on('entry', function(entry) {
-                    if (entry.path === db)
-                      entry.pipe(fs.createWriteStream(db)).on('finish', success)
-                    else
-                      entry.autodrain()
-                  })
-              }
+function unzip(callback) {
+  fs.unlink(DB, function() {
+    spawn('unzip', ['-j', TMP, DB, '-d', __dirname])
+      .on('error', callback)
+      .on('exit', function() {
+        callback()
+      })
+  })
+}
+
+function nodeUnzip(callback) {
+  var npm = require('npm');
+  npm.load(function(err) {
+    npm.commands.install(['unzip'], function(err, data) {
+      fs.createReadStream(TMP)
+        .pipe(require('unzip').Parse())
+        .on('entry', function(entry) {
+          if (entry.path === DB)
+            entry.pipe(fs.createWriteStream(DB)).on('finish', function() {
+              callback();
             })
+          else
+            entry.autodrain()
         })
-        .on('close', success)
+        .on('error', callback)
     })
   })
+}
+
+function success() {
+  console.info('Database successfully installed')
+  fs.unlink(TMP, process.exit)
+}
+
+
+download(function(err) {
+  if (err)
+    return console.error('Unable to download database from ipip.net')
+
+  unzip(function(err) {
+    if (err) {
+      console.warn('zip command not found, fallback to node-unzip')
+      return nodeUnzip(function(err) {
+        if (err)
+          return console.error('Unexpected error occured while extracting database')
+        else
+          success()
+      })
+    }
+
+    success()
+  })
+
 })
